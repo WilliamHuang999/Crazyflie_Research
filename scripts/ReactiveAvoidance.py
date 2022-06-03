49d6b52a7c6a408a9cd3d2c161ef2a0bc904ba5a
davidfu81 <davidfu100@gmail.com>
2022-06-03 13:13:14 -0400

    Update ReactiveAvoidance.py

--- scripts/ReactiveAvoidance.py
+++ scripts/ReactiveAvoidance.py
@@ -16,6 +16,7 @@ device_product_line = str(device.get_info(rs.camera_info.product_line))
 
 # Establish depth stream
 config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
 # Start streaming
 pipeline.start(config)
@@ -32,7 +33,7 @@ try:
         frames = pipeline.wait_for_frames()
         depth_frame = frames.get_depth_frame()
         color_frame = frames.get_color_frame()
-        if not depth_frame:
+        if not depth_frame or not color_frame:
             continue
 
         # Convert image to numpy arrays

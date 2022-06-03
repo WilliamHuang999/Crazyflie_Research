f117fb614be938c0c1277729f9acfdac86f8b5e7
davidfu81 <davidfu100@gmail.com>
2022-06-03 14:01:14 -0400

    Update ReactiveAvoidance.py

--- scripts/ReactiveAvoidance.py
+++ scripts/ReactiveAvoidance.py
@@ -45,7 +45,7 @@ try:
         trimmed_depth_image = depth_image[(int)(IMG_HEIGHT/10):(int)(IMG_HEIGHT*9/10), \
                                     (int)(IMG_WIDTH/10):(int)(IMG_WIDTH*9/10)]   #Trim off edges of depth image
         [TRIMMED_HEIGHT, TRIMMED_WIDTH] = np.shape(trimmed_depth_image)
-        trimmed_depth_image = cv.convertScaleAbs(trimmed_depth_image) #Convert to 8 bit values
+        trimmed_depth_image = cv.convertScaleAbs(trimmed_depth_image, alpha=(255.0/65535.0)) #Convert to 8 bit values
         proccessed_depth_image = cv.bilateralFilter(trimmed_depth_image, 5, 75, 75) #Noise Reduction
         
 

f2f83368e6cbe15b0ee2387b16ad91982a1843bc
johnnwallace <jw0535@princeton.edu>
2022-06-06 13:59:05 -0400

    Update ReactiveAvoidanceV2.py

--- scripts/ReactiveAvoidanceV2.py
+++ scripts/ReactiveAvoidanceV2.py
@@ -29,11 +29,11 @@ pipeline.start(config)
 stopDist = 0.5  # Distance in meters away from an object that prevents the drone from moving forward
 lastTime = time.time()
 
-middle_running_average = np.empty((1,IMG_WIDTH))
+middle_running_average = np.empty((1, IMG_WIDTH))
 target_running_average = []
 
-ceiling_m = 2 # ceiling in meters
-meters_per_pixel = 2*ceiling_m/IMG_WIDTH * np.tan(0.5 * np.radians(FOV))
+ceiling_m = 2  # ceiling in meters
+meters_per_pixel = 2 * ceiling_m / IMG_WIDTH * np.tan(0.5 * np.radians(FOV))
 
 try:
     while True:
@@ -50,26 +50,26 @@ try:
         color_image = np.asanyarray(color_frame.get_data())
 
         # Take middle slice of image
-        middle_depth = depth_image[(int)(IMG_HEIGHT/2)-10:(int)(IMG_HEIGHT/2)+10, :]
-        middle_depth_averages = np.mean(middle_depth, axis = 0)
+        middle_depth = depth_image[(int)(IMG_HEIGHT / 2) - 10 : (int)(IMG_HEIGHT / 2) + 10, :]
+        middle_depth_averages = np.mean(middle_depth, axis=0)
 
-        
-        if np.size(middle_running_average[:,0]) < 10: 
+        if np.size(middle_running_average[:, 0]) < 10:
             middle_running_average = np.vstack((middle_running_average, middle_depth_averages))
         else:
-            middle_running_average = middle_running_average[1:,:]
+            middle_running_average = middle_running_average[1:, :]
             middle_running_average = np.vstack((middle_running_average, middle_depth_averages))
 
-        middle_depth_filtered = np.mean(middle_running_average, axis = 0)
-        
-        
+        middle_depth_filtered = np.mean(middle_running_average, axis=0)
+
         # Find largest gap above depth ceiling
-        ceiling = ceiling_m/depth_frame.get_units() # in RealSense depth units
+        ceiling = ceiling_m / depth_frame.get_units()  # in RealSense depth units
         count = 0
         longest = -1
         longestStart = -1
         longestEnd = -1
         for i in range(0, np.size(middle_depth_filtered)):
+            if middle_depth_filtered[i] < ceiling:
+                middle_depth_filtered[i] = 0
             if middle_depth_filtered[i] > ceiling:
                 middle_depth_filtered[i] = 65535
                 count += 1
@@ -84,34 +84,33 @@ try:
             # stop drone
             gapCenter = 0
         else:
-            gapCenter = (int)((longestStart + longestEnd)/2)
+            gapCenter = (int)((longestStart + longestEnd) / 2)
 
         # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
         depth_colormap = cv.applyColorMap(cv.convertScaleAbs(depth_image, alpha=0.03), cv.COLORMAP_JET)
         depth_colormap_dim = depth_colormap.shape
-        cv.circle(depth_colormap, (gapCenter, (int)(IMG_HEIGHT/2)), 10, (0, 0, 0), 3) #Black
+        cv.circle(depth_colormap, (gapCenter, (int)(IMG_HEIGHT / 2)), 10, (0, 0, 0), 3)  # Black
 
-        
         # Make colormap of middle_depth_averages
         middle_depth_average_expanded = np.empty((IMG_HEIGHT, IMG_WIDTH))
         for i in range(0, IMG_HEIGHT):
             middle_depth_average_expanded[i] = middle_depth_filtered
-        middle_depths_colormap = cv.applyColorMap(\
-            cv.convertScaleAbs(middle_depth_average_expanded, alpha = 0.03), cv.COLORMAP_JET)
-        #cv.circle(middle_depths_colormap, (gapCenter, (int)(IMG_HEIGHT/2)), 10, (0, 0, 0), 3) #Black
+        middle_depths_colormap = cv.applyColorMap(
+            cv.convertScaleAbs(middle_depth_average_expanded, alpha=0.03), cv.COLORMAP_JET
+        )
+        # cv.circle(middle_depths_colormap, (gapCenter, (int)(IMG_HEIGHT/2)), 10, (0, 0, 0), 3) #Black
 
         # Show images
         cv.imshow("Original DepthMap", depth_colormap)
         cv.imshow("RGB", color_image)
-        cv.imshow("Center Depths", cv.convertScaleAbs(middle_depth_average_expanded, alpha = 0.03))
+        cv.imshow("Center Depths", cv.convertScaleAbs(middle_depth_average_expanded, alpha=0.03))
 
-        #print(middle_depth_averages[(int)(IMG_WIDTH/2)]*depth_frame.get_units())
+        # print(middle_depth_averages[(int)(IMG_WIDTH/2)]*depth_frame.get_units())
 
-        if cv.waitKey(1) == ord('q'):
+        if cv.waitKey(1) == ord("q"):
             break
 
 finally:
 
     # Stop streaming
     pipeline.stop()
-

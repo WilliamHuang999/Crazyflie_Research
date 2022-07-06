cfdf7c165bf17a359d9100c3e68a63db2dfbfb6f
johnnwallace <jw0535@princeton.edu>
2022-07-06 15:41:15 -0400

    Added rateType control mode

--- firmware/rateController/src/drivers/interface/led.h
+++ firmware/rateController/src/drivers/interface/led.h
@@ -1,6 +1,6 @@
 /**
- *    ||          ____  _ __                           
- * +------+      / __ )(_) /_______________ _____  ___ 
+ *    ||          ____  _ __
+ * +------+      / __ )(_) /_______________ _____  ___
  * | 0xBC |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
  * +------+    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
  *  ||  ||    /_____/_/\__/\___/_/   \__,_/ /___/\___/
@@ -28,37 +28,49 @@
 
 #include <stdbool.h>
 
-//Led polarity configuration constant
+// Led polarity configuration constant
 #define LED_POL_POS 0
 #define LED_POL_NEG 1
 
-//Hardware configuration
-#define LED_GPIO_PERIF   (RCC_AHB1Periph_GPIOC | RCC_AHB1Periph_GPIOD)
-#define LED_GPIO_PORT_BLUE  GPIOD
-#define LED_GPIO_BLUE_L  GPIO_Pin_2
-#define LED_POL_BLUE_L   LED_POL_POS
-#define LED_GPIO_PORT    GPIOC
+// Hardware configuration
+#define LED_GPIO_PERIF (RCC_AHB1Periph_GPIOC | RCC_AHB1Periph_GPIOD)
+#define LED_GPIO_PORT_BLUE GPIOD
+#define LED_GPIO_BLUE_L GPIO_Pin_2
+#define LED_POL_BLUE_L LED_POL_POS
+#define LED_GPIO_PORT GPIOC
 #define LED_GPIO_GREEN_L GPIO_Pin_1
-#define LED_POL_GREEN_L  LED_POL_NEG
-#define LED_GPIO_RED_L   GPIO_Pin_0
-#define LED_POL_RED_L    LED_POL_NEG
+#define LED_POL_GREEN_L LED_POL_NEG
+#define LED_GPIO_RED_L GPIO_Pin_0
+#define LED_POL_RED_L LED_POL_NEG
 #define LED_GPIO_GREEN_R GPIO_Pin_2
-#define LED_POL_GREEN_R  LED_POL_NEG
-#define LED_GPIO_RED_R   GPIO_Pin_3
-#define LED_POL_RED_R    LED_POL_NEG
+#define LED_POL_GREEN_R LED_POL_NEG
+#define LED_GPIO_RED_R GPIO_Pin_3
+#define LED_POL_RED_R LED_POL_NEG
 
-#define LINK_LED         LED_GREEN_L
-#define CHG_LED          LED_BLUE_L
-#define LOWBAT_LED       LED_RED_R
-#define LINK_DOWN_LED    LED_RED_L
-#define SYS_LED          LED_RED_R
-#define ERR_LED1         LED_RED_L
-#define ERR_LED2         LED_RED_R
+#define LINK_LED LED_GREEN_L
+#define CHG_LED LED_BLUE_L
+#define LOWBAT_LED LED_RED_R
+#define LINK_DOWN_LED LED_RED_L
+#define SYS_LED LED_GREEN_R
+#define ERR_LED1 LED_RED_L
+#define ERR_LED2 LED_RED_R
 
 #define LED_NUM 6
 
-typedef enum {LED_BLUE_L = 0, LED_GREEN_L, LED_RED_L, LED_GREEN_R, LED_RED_R, LED_BLUE_NRF} led_t;
-typedef enum { LED_LEDSEQ, LED_PARAM_BITMASK } ledSwitch_t;
+typedef enum
+{
+    LED_BLUE_L = 0,
+    LED_GREEN_L,
+    LED_RED_L,
+    LED_GREEN_R,
+    LED_RED_R,
+    LED_BLUE_NRF
+} led_t;
+typedef enum
+{
+    LED_LEDSEQ,
+    LED_PARAM_BITMASK
+} ledSwitch_t;
 
 void ledInit();
 bool ledTest();
@@ -75,7 +87,7 @@ void ledSet(led_t led, bool value);
 // Shoes fault pattern (2 Red ON, 2 Green and Blue OFF)
 void ledShowFaultPattern(void);
 
-//Legacy functions
+// Legacy functions
 #define ledSetRed(VALUE) ledSet(LED_RED, VALUE)
 #define ledSetGreen(VALUE) ledSet(LED_GREEN, VALUE)
 

77e15c6df35abfa3ba648e7842fa13d1a9e82174
johnnwallace <jw0535@princeton.edu>
2022-07-07 13:48:19 -0400

    Added trajectory example

--- scripts/Test.py
+++ scripts/Test.py
@@ -6,10 +6,15 @@ import sys
 import time
 from threading import Event
 import struct
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+# import pandas
 
 import cflib.crtp
 from cflib.crazyflie import Crazyflie
 from cflib.crazyflie.log import LogConfig
+from cflib.crazyflie.syncLogger import SyncLogger
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 from cflib.positioning.motion_commander import MotionCommander
 from cflib.crazyflie.commander import Commander
@@ -18,24 +23,62 @@ from cflib.utils import uri_helper
 from cflib.crtp.crtpstack import CRTPPacket
 from cflib.crtp.crtpstack import CRTPPort
 
+debug = False
+
+
+class data:
+    t = 0
+    roll = 0
+    pitch = 0
+    yaw = 0
+    thrust = 0
+
+    def toString(self):
+        return f"{myData.t}: {round(myData.roll, 2)}, {round(myData.pitch, 2)}, {round(myData.yaw, 2)}, {round(myData.thrust, 2)}"
+
+
+myData = data()
 
 # uri = "usb://0"
 uri = "radio://0/80/2M/"
 
+# Only output errors from the logging framework
+logging.basicConfig(level=logging.ERROR)
+
 baseAlt = 0.5
+
+# Add log config for asynchronous logging
+def simple_log_async(scf, logconf):
+    cf = scf.cf
+    cf.log.add_config(logconf)
+    logconf.data_received_cb.add_callback(log_stab_callback)
+
+
+# Logging callback function
+def log_stab_callback(timestamp, data, logconf):
+    myData.t = timestamp
+    myData.roll = data["stabilizer.roll"]
+    myData.pitch = data["stabilizer.pitch"]
+    myData.yaw = data["stabilizer.yaw"]
+    myData.thrust = data["stabilizer.thrust"]
+
+    if debug:
+        print("[%d][%s]: %s" % (timestamp, logconf.name, data))
+
 
 # Ascend to a given height in a certain number of steps
 def ascend(cf, alt, steps):
-    for y in range(alt * steps):
+    for y in range(round(alt * steps)):
         cf.commander.send_hover_setpoint(0, 0, 0, y / steps)
         time.sleep(0.1)
 
 
 # Set angular rate setpoints
+# Not working -- unclear whether the problem is this or the modified firmware
 def send_rates(cf, roll_rate, pitch_rate, yaw_rate):
     pk = CRTPPacket()
     pk.port = CRTPPort.COMMANDER_GENERIC
-    pk.data = struct.pack("<Bffff", 8, roll_rate, pitch_rate, yaw_rate)
+    pk.data = struct.pack("<Bfff", 8, roll_rate, pitch_rate, yaw_rate)
     cf.send_packet(pk)
 
 
@@ -52,6 +95,13 @@ def param_deck(id, value_str):
 # Initialize all the CrazyFlie drivers:
 cflib.crtp.init_drivers(enable_debug_driver=False)
 
+# Configure logging
+lg_stab = LogConfig(name="Stabilizer", period_in_ms=10)
+lg_stab.add_variable("stabilizer.roll", "float")
+lg_stab.add_variable("stabilizer.pitch", "float")
+lg_stab.add_variable("stabilizer.yaw", "float")
+lg_stab.add_variable("stabilizer.thrust", "float")
+
 # Scan for Crazyflies in range of the antenna:
 print("Scanning interfaces for Crazyflies...")
 available = cflib.crtp.scan_interfaces()
@@ -63,13 +113,35 @@ for i in available:
 
 # Check that CrazyFlie devices are available:
 if len(available) == 0:
-    print("No Crazyflies found, cannot run example")
+    print("No Crazyflies found")
 else:
     with SyncCrazyflie(uri, cf=Crazyflie(rw_cache="./cache")) as scf:
         cf = scf.cf
         cf.param.add_update_callback(group="deck", name="bcFlow2", cb=param_deck)
+        simple_log_async(scf, lg_stab)
+        lg_stab.start()
 
-        time.sleep(2)
+        # ascend(cf, baseAlt, 10)
+        # cf.commander.send_setpoint()
 
-        ascend(cf, baseAlt, 10)
-        cf.commander.send_setpoint()
+        t0 = time.time()
+        elapsed = 0
+
+        # Main loop
+        while elapsed < 5:
+            cf.commander.send_hover_setpoint(0, 0, 0, 1)
+
+            print(myData.toString())
+            elapsed = time.time() - t0
+            time.sleep(0.05)
+
+        while elapsed < 7:
+            cf.commander.send_setpoint(100, 100, 100, 40000)
+
+            print(myData.toString())
+            elapsed = time.time() - t0
+            time.sleep(0.05)
+
+        cf.commander.send_hover_setpoint(0, 0, 0, 0)
+
+        lg_stab.stop()
